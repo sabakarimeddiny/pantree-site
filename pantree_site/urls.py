acb06cf465dc870b6c0cf88a1c8bd63b367b80de
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-07-08 12:00:37 -0400

    made home url app

--- pantree_site/urls.py
+++ pantree_site/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('pantree_app.urls')),
     path('admin/', admin.site.urls),
-    path('pantree/', include('pantree_app.urls'))
+    # path('pantree/', include('pantree_app.urls'))
 ]
