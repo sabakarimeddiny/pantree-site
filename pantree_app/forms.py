2ec95ff31dfd8908a4c23606120c2dd9fe8bc9d7
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-07-12 14:00:26 -0400

    Update forms.py

--- pantree_app/forms.py
+++ pantree_app/forms.py
@@ -4,9 +4,9 @@ class NameForm(forms.Form):
     your_name = forms.CharField(label='Your Name: ', max_length=100)
 
 class IngredientForm(forms.Form):
-    ingredients = forms.CharField(label='ingredients I have', max_length=1000)
-    must_have_ings = forms.CharField(label = 'show me only recipes that have (optional)', max_length=1000, initial='')
-    max_missing_ings = forms.IntegerField(label='max. number of missing ingredients', max_value=50, required=False)
+    ingredients = forms.CharField(label='ingredients I have', max_length=10000, initial='', required=False)
+    must_have_ings = forms.CharField(label = 'only show me only recipes that have (optional)', max_length=1000, initial='', required=False)
+    max_missing_ings = forms.IntegerField(label='max. number of missing ingredients (optional)', max_value=100, required=False)
 
 # class IntForm(forms.Form):
     
