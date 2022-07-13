1824bc3df5ad49bdce46b8c82156d3a0e7bbda68
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-07-13 16:30:49 -0400

    broke down recipeBank object

--- pantree_app/src/pantree/recipe.py
+++ pantree_app/src/pantree/recipe.py
@@ -96,39 +96,10 @@ class recipeDB:
 
 class recipeBank:
 
-    def __init__(self, json_recipe_bank = ''):
-        if json_recipe_bank == '':
-            self.data = None
-            self.urls = None
-        else:
-            with open(json_recipe_bank) as json_file:
-                self.recipes = json.load(json_file)
-                xss = list(self.recipes.values())
-                self.ingredients = sorted(set([x for xs in xss for x in xs]))
-                self.urls = sorted(set(list(self.recipes.keys())))
-                self.data = None
-    
-    def make_sparse_matrix(self):
-        self.data = dok_matrix((len(self.ingredients), len(self.urls)), dtype=np.int8)
-        i = 0
-        urls = []
-        for k, v in self.recipes.items():
-            one_hot = np.array(common.vectorize(v, self.ingredients))
-            one_hot_indices = np.where(one_hot == 1)
-            for j in one_hot_indices:
-                self.data[j, i] = 1
-            i+=1
-            urls.append(k)
-        self.urls = urls
-    
-    def save(self, fname):
-        with open(os.path.join(fname, 'matrix.p'), 'wb') as f:
-            pickle.dump(self.data, f)
-        with open(os.path.join(fname, 'urls.p'), 'wb') as f:
-            pickle.dump(self.urls, f)
-        with open(os.path.join(fname, 'ingredients.p'), 'wb') as f:
-            pickle.dump(self.ingredients, f)
-    
+    def __init__(self):
+        self.data = None
+        self.urls = None
+        
 
 
     
