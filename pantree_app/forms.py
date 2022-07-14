94d47516529e1e1d93e08ccd43075f05d4623c6d
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-07-14 11:10:40 -0400

    removed initial fill

--- pantree_app/forms.py
+++ pantree_app/forms.py
@@ -4,8 +4,8 @@ class NameForm(forms.Form):
     your_name = forms.CharField(label='Your Name: ', max_length=100)
 
 class IngredientForm(forms.Form):
-    ingredients = forms.CharField(label='ingredients I have', max_length=10000, initial='', required=False)
-    must_have_ings = forms.CharField(label = 'only show me only recipes that have (optional)', max_length=1000, initial='', required=False)
+    ingredients = forms.CharField(label='ingredients I have', max_length=10000, required=False)
+    must_have_ings = forms.CharField(label = 'only show me only recipes that have (optional)', max_length=1000, required=False)
     max_missing_ings = forms.IntegerField(label='max. number of missing ingredients (optional)', max_value=100, required=False)
 
 # class IntForm(forms.Form):
