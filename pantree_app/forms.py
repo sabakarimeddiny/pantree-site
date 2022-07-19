92a53eb3b4f144ebd35e709062fcc3363a66feed
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-07-19 14:54:24 -0400

    changed labels to box placeholders

--- pantree_app/forms.py
+++ pantree_app/forms.py
@@ -4,8 +4,10 @@ class NameForm(forms.Form):
     your_name = forms.CharField(label='Your Name: ', max_length=100)
 
 class IngredientForm(forms.Form):
-    ingredients = forms.CharField(label='ingredients I have', max_length=10000, required=False)
-    must_have_ings = forms.CharField(label = 'only show me recipes that have', max_length=1000, required=False)
+    ingredients = forms.CharField(label='', max_length=10000, required=False)
+    ingredients.widget.attrs.update({'placeholder': 'Ingredients to Add'})
+    must_have_ings = forms.CharField(label = '', max_length=1000, required=False)
+    must_have_ings.widget.attrs.update({'placeholder': 'Ingredients to Require'})
     # max_missing_ings = forms.IntegerField(label='max. number of missing ingredients (optional)', max_value=100, required=False)
 
 # class IntForm(forms.Form):
