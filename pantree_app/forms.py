b87feb04d9f56325f28dbe908726a8e0e757aa57
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-07-22 10:04:34 -0400

    placeholder updated

--- pantree_app/forms.py
+++ pantree_app/forms.py
@@ -5,7 +5,7 @@ class NameForm(forms.Form):
 
 class IngredientForm(forms.Form):
     ingredients = forms.CharField(label='', max_length=10000, required=False)
-    ingredients.widget.attrs.update({'placeholder': 'Ingredient'})
+    ingredients.widget.attrs.update({'placeholder': 'Add Each Ingredient'})
     must_have_ings = forms.CharField(label = '', max_length=1000, required=False)
     must_have_ings.widget.attrs.update({'placeholder': 'Ingredients to Require'})
     # max_missing_ings = forms.IntegerField(label='max. number of missing ingredients (optional)', max_value=100, required=False)
