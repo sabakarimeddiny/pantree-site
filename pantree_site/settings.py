c52f716562aaf68a04cbd2ce3fde1526c937401f
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-07-27 09:44:07 -0400

    moved to amazon db

--- pantree_site/settings.py
+++ pantree_site/settings.py
@@ -76,15 +76,35 @@ TEMPLATES = [
 WSGI_APPLICATION = 'pantree_site.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+# if 'RDS_HOSTNAME' in os.environ:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.mysql',
+#             'NAME': os.environ['RDS_DB_NAME'],
+#             'USER': os.environ['RDS_USERNAME'],
+#             'PASSWORD': os.environ['RDS_PASSWORD'],
+#             'HOST': os.environ['RDS_HOSTNAME'],
+#             'PORT': os.environ['RDS_PORT'],
+#         }
+#     }
+# else:
 
+# local database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': str(BASE_DIR / 'db.sqlite3'),
+#     }
+# }
+# Production database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': str(BASE_DIR / 'db.sqlite3'),
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ebdb',
+        'USER': 'sabakarimeddiny',
+        'PASSWORD': 'saba1294',
+        'HOST': 'aa18m77akxru1c5.cu3o6orh213n.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',}}
 
 
 # Custom User
