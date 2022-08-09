d1253cff3792bd165d4bce67662deaa49a0985db
Saba Karimeddiny <sabakarimeddiny@gmail.com>
2022-08-09 13:30:50 -0400

    static updates

--- static/js/dragndrop.js
+++ static/js/dragndrop.js
@@ -1,11 +1,31 @@
+// $(document).ready( function () {
+//     $('.list[id=ing], .list[id=must_have]').droppable({
+//         // tolerance: 'fit'
+//         tolerance: "intersect",
+//         drop: function (event, ui) {
+//             var ob = ui.draggable;
+//             $(this).append(ob.css({position: 'static'}));
+//             // ob.css({position: 'relative'});
+//         }
+//     });
+// });
+
 $(document).ready( function () {
     $('.list[id=ing], .list[id=must_have]').droppable({
-        // tolerance: 'fit'
         tolerance: "intersect",
         drop: function (event, ui) {
             var ob = ui.draggable;
-            $(this).append(ob.css({position: 'static'}));
-            // ob.css({position: 'relative'});
+            ob.css({position: 'static'})
+            var targetList = $(this)
+            var added = false;
+            $(".removable-list-item", targetList).each(function(){
+                if ($(this).text() > $(ob).text()) {
+                    $(ob).insertBefore($(this)).fadeIn("fast");
+                    added = true;
+                    return false;
+                }
+            });
+            if(!added) $(ob).appendTo($(targetList)).fadeIn("fast");
         }
     });
 });
